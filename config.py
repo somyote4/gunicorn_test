0af8e6a3cf25d7ab72d47e3ac9c4364213c41f6b
somyote4 <somyote.4@gmail.com>
2019-10-16 04:42:07 +0000

    0.11 commit

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-import os, multiprocessing
+import os#, multiprocessing
 
 
-bind = "192.168.11.17:8000"
+#bind = "0.0.0.0:8000"
 
 
 workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
@@ -10,5 +10,5 @@ threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 forwarded_allow_ips = '*'
 secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
 
-BLOG_NAME='Blog'
+#BLOG_NAME='Blog'
 

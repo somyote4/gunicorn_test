e5a8cc75742653ce87b989b460ded8fb93c07932
somyote4 <somyote.4@gmail.com>
2019-10-16 19:11:51 +0700

    0.16 commit

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,14 @@
-#from myproject import application
-
-#if __name__ == "__main__":
-#    application.run()
-from flask import Flask
-application = Flask(__name__)
-
-@application.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+from myproject import application
 
 if __name__ == "__main__":
     application.run()
+
+#from flask import Flask
+#application = Flask(__name__)
+
+#@application.route("/")
+#def hello():
+#   return "<h1 style='color:blue'>Hello There!</h1>"
+
+#if __name__ == "__main__":
+#    application.run()

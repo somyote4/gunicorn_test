199f70f564a6ff4ec97e28d3904d726f36d88b69
somyote4 <somyote.4@gmail.com>
2019-10-16 18:19:37 +0700

    0.15 commit

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,13 @@
-from myproject import application
+#from myproject import application
+
+#if __name__ == "__main__":
+#    application.run()
+from flask import Flask
+application = Flask(__name__)
+
+@application.route("/")
+def hello():
+    return "Hello World!"
 
 if __name__ == "__main__":
     application.run()
